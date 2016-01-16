0ed78ee3d7860b3edee829ca67fa97034f6f4576
Keith Mosher <kmosher@yelp.com>
2016-01-16 02:40:51 -0800

    Switch to using a dotfiles script
    sourced from github.com/cowboy/dotfiles

---
+++ init/30_osx_homebrew_casks.sh
@@ -0,0 +1,43 @@
+# OSX-only stuff. Abort if not OSX.
+is_osx || return 1
+
+# Exit if Homebrew is not installed.
+[[ ! "$(type -P brew)" ]] && e_error "Brew casks need Homebrew to install." && return 1
+
+# Ensure the cask keg and recipe are installed.
+kegs=(caskroom/cask)
+brew_tap_kegs
+
+# Hack to show the first-run brew-cask password prompt immediately.
+brew cask info this-is-somewhat-annoying 2>/dev/null
+
+# Homebrew casks
+casks=(
+  # Applications
+  aquamacs
+  battle-net
+  dropbox
+  firefox
+  google-chrome
+  iterm2
+  slack
+  spotify
+  steam
+  the-unarchiver
+  xquartz
+  yubico-authenticator
+  yubikey-personalization-gui
+  vagrant
+  virtualbox
+  vlc
+)
+
+# Install Homebrew casks.
+casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
+if (( ${#casks[@]} > 0 )); then
+  e_header "Installing Homebrew casks: ${casks[*]}"
+  for cask in "${casks[@]}"; do
+    brew cask install $cask
+  done
+  brew cask cleanup
+fi
---
+++ init/50_emacs.sh
@@ -0,0 +1,1 @@
+~/.cask/bin/cask --path ~/.emacs.d/
