1821bca4d609b230258a1c99b6f88ce6d3ebc001
Keith Mosher <kmosher@yelp.com>
2016-03-08 20:37:04 -0800

    Add optional homebrew token

--- link/.zsh.d/environ.zsh
+++ link/.zsh.d/environ.zsh
@@ -1,3 +1,7 @@
 # Setup editor to use emacs if the server is running
 export VISUAL='emacsclient -t --alternate-editor=vim'
 export EDITOR='emacsclient -t --alternate-editor=vim'
+
+if [ -f ~/.homebrew_api_token ]; then
+   export HOMEBREW_GITHUB_API_TOKEN=$(cat ~/.homebrew_api_token)
+fi
