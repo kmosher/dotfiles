bce3428b92e0559b01b9c0a98f3c09615ce70cae
Keith Mosher <kmosher@yelp.com>
2016-10-06 19:21:27 -0700

    Switch to a minmalist prompt

--- link/.zsh.d/prompt.zsh
+++ link/.zsh.d/prompt.zsh
@@ -1,7 +1,10 @@
 autoload -U colors && colors
 
-PROMPT="%{${fg[blue]}%}%B%n%b%{${fg[default]}%}@%{$fg[magenta]%}%B%m%b%{$fg[default]%}:%{$fg[yellow]%}%B%~%b%{$fg[default]%}%# "
-RPROMPT="%{$fg[red]%}[%* on %D]%{$fg[default]%}"
+#PROMPT="%{${fg[blue]}%}%B%n%b%{${fg[default]}%}@%{$fg[magenta]%}%B%m%b%{$fg[default]%}:%{$fg[yellow]%}%B%~%b%{$fg[default]%}%# "
+#RPROMPT="%{$fg[red]%}[%* on %D]%{$fg[default]%}"
+
+PROMPT="%{$fg[red]%}[%* on %D]%{$fg[default]%} %{$fg[yellow]%}%B%~%b%{$fg[default]%}%# %{$fg[red]%}"
+#RPROMPT="%{$fg[red]%}[%* on %D]%{$fg[default]%}"
 
 # TODO: Redo as the zsh ternary syntax
 if [ "$YELP_IN_SANDBOX" ]; then
