2aa1ddb2c11c4b8e953ab45942e81d698a5d9e05
Keith Mosher <kmosher@yelp.com>
2017-08-01 16:07:50 -0700

    Fix typos in pip

--- init/40_pip.sh
+++ init/40_pip.sh
@@ -1,9 +1,9 @@
 pip2.7 install --upgrade pip
 pip3 install --upgrade pip
 
-pip2.7 install --upgarde ipdb
-pip3 install --upgarde ipdb
+pip2.7 install --upgrade ipdb
+pip3 install --upgrade ipdb
 
-pip3 install --upgarde rainbowstream
+pip3 install --upgrade rainbowstream
 pip3 install --upgrade awscli
 pip3 install --upgrade aws-roleshell
