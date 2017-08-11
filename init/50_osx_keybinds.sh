f82fcee85634e4afd23cf2eaacda2d2c59117910
Keith Mosher <kmosher@yelp.com>
2017-08-10 20:33:23 -0700

    Add ssh-agent on boot

--- init/50_osx_keybinds.sh
+++
@@ -1,4 +0,0 @@
-is_osx || return 1
-
-mkdir -p ~/Library/KeyBindings/
-ln -v "$DOTFILES/conf/osx/Library/KeyBindings/DefaultKeyBinding.dict" ~/Library/KeyBindings
--- init/50_osx_settings.sh
+++ init/50_osx_settings.sh
@@ -12,4 +12,10 @@ defaults write ~/Library/Preferences/com.apple.dock wvous-tr-modifier -int 1
 defaults write ~/Library/Preferences/com.apple.dock wvous-tl-corner -int 5
 defaults write ~/Library/Preferences/com.apple.dock wvous-tl-modifier -int 1
 
+mkdir -p ~/Library/KeyBindings/
+ln -v "$DOTFILES/conf/osx/Library/KeyBindings/DefaultKeyBinding.dict" ~/Library/KeyBindings
+
+ln -v "$DOTFILES/conf/osx/Library/LaunchAgents/org.homebrew.ssh-agent.plist" ~/Library/LaunchAgents/
+launchctl load -w ~/Library/LaunchAgents/org.homebrew.ssh-agent.plist
+
 echo "Logout and login for OSX settings to take effect"
