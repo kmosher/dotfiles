1659f0c75815e99662b194d15134a306ae4f9072
Keith Mosher <keith.mosher@gmail.com>
2018-11-20 16:40:58 -0800

    Make LS_COLORS userland agnostic

--- link/.zsh.d/ls.zsh
+++ link/.zsh.d/ls.zsh
@@ -1,19 +1,13 @@
 # ls colors
-autoload colors; colors;
+autoload colors && colors;
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
 eval $(dircolors -b $HOME/.zsh.d/dircolors)
 
 # Enable ls colors
 if [ "$DISABLE_LS_COLORS" != "true" ]
 then
-  # Find the option for using colors in ls, depending on the version: Linux or BSD
-  if [[ "$(uname -s)" == "NetBSD" ]]; then
-    # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
-    # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
-    gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
-  else
-    ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
-  fi
+    # Find the option for using colors in ls, depending on the version: Linux or BSD
+    ls --color -d /dev/null &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
 fi
 
 # List direcory contents
