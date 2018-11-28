03a47ccc3d61cb3ce2f082fe465c5b5706cb6761
Keith Mosher <keith.mosher@gmail.com>
2018-11-27 21:48:11 -0800

    Disable lsp-mode

--- link/.emacs.d/personal/kmosher-ac.el
+++ link/.emacs.d/personal/kmosher-ac.el
@@ -2,5 +2,6 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
-(require 'company-lsp)
-(push 'company-lsp company-backends)
+; I think this is causing crazy lag for me
+;(require 'company-lsp)
+;(push 'company-lsp company-backends)
--- link/.emacs.d/personal/kmosher-vue.el
+++ link/.emacs.d/personal/kmosher-vue.el
@@ -3,12 +3,12 @@
 (add-hook 'vue-mode-hook (lambda ()
   (setq mmm-submode-decoration-level 0)
 ;  This seems to die with "Marker does not point anywhere"
-;  (setq mmm-parse-when-idle t)
+  (setq mmm-parse-when-idle t)
 ))
 
-(require 'lsp-mode)
-(require 'lsp-vue)
-(add-hook 'vue-mode-hook #'lsp-vue-mmm-enable)
-
-(require 'lsp-ui)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+;; Vueter just isn't built for dogshit slow WSL filesystem
+;(require 'lsp-mode)
+;(require 'lsp-vue)
+;(add-hook 'vue-mode-hook #'lsp-vue-mmm-enable)
+;(require 'lsp-ui)
+;(add-hook 'lsp-mode-hook 'lsp-ui-mode)
